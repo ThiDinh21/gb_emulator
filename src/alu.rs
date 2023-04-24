e7de479f4ac86b546a556a3c324d34fdb9ef7d70
Thi Dinh <vietthidinh2001@gmail.com>
2023-04-24 18:45:20 +0300

    add: todo func for alu crate

---
+++ src/alu.rs
@@ -0,0 +1,15 @@
+pub fn add_u8(x: u8, y: u8, carry: bool) -> (u8, bool, bool, bool) {
+    todo!()
+}
+
+pub fn add_u16(x: u16, y: u16, carry: bool) -> (u16, bool, bool, bool) {
+    todo!()
+}
+
+pub fn sub_u8(x: u8, y: u8, carry: bool) -> (u8, bool, bool, bool) {
+    todo!()
+}
+
+pub fn sub_u16(x: u16, y: u16, carry: bool) -> (u16, bool, bool, bool) {
+    todo!()
+}
