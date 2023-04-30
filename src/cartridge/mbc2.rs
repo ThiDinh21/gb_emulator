d212385c30386888e3df92034e48fe0c319fac4e
Thi Dinh <vietthidinh2001@gmail.com>
2023-04-30 05:05:33 +0300

    add: mbc2.rs file (incomplete)

--- src/cartridge/mbc2.rs
+++ src/cartridge/mbc2.rs
@@ -1,7 +1,26 @@
+use std::path::PathBuf;
+
 use crate::cartridge::MBC;
 
 pub struct MBC2 {
     rom: Vec<u8>,
     ram: Vec<u8>,
     rom_bank_idx: usize,
+    ram_enabled: bool,
+    save_file: PathBuf,
+}
+
+impl MBC2 {
+    pub fn new(raw: Vec<u8>, path: PathBuf) -> Result<Self, &'static str> {
+        let save_file = path.with_extension("save");
+        let ram = todo!();
+
+        Ok(MBC2 {
+            rom: (),
+            ram: (),
+            rom_bank_idx: (),
+            ram_enabled: (),
+            save_file: (),
+        })
+    }
 }
